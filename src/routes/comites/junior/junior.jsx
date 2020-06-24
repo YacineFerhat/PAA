0212afd312acde4970b8a85a641d0f3798f00777
Yacine Ferhat <Yacine.frhat@gmail.com>
2020-06-24 15:47:36 +0100

    Ajout du comité junior / sport

--- src/routes/comites/junior/junior.jsx
+++ src/routes/comites/junior/junior.jsx
@@ -1,7 +1,103 @@
 import React from "react";
 
 const Junior = () => {
-  return <section>Lorem</section>;
+  return (
+    <section className="hero ">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-6">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+            <div className="column">
+              <h1 className="title is-4" style={{ marginTop: "14%" }}>
+                Comité Junior
+              </h1>
+              <p style={{ textIndent: "20px", lineHeight: "35px" }}>
+                Dédié uniquement aux <strong>enfants </strong>, le comité Junior
+                sous son initiative baptisée <strong>« NWA3I WLIDI »</strong> ou
+                autrement <strong>« JE SENSIBILISE MON ENFANT »</strong>, a pour
+                principal but la <strong>sensibilisation</strong> des enfants à
+                l’importance de la{" "}
+                <strong>protection de l’environnement</strong>.
+              </p>
+              <br />
+              <br />
+              <p style={{ textIndent: "20px", lineHeight: "35px" }}>
+                Conscient de <strong>l’importance</strong> du rôle de l’enfant
+                d’aujourd’hui sur l’environnement demain, l’équipe œuvre à la
+                sensibilisation à travers différentes activités{" "}
+                <strong>écologiques</strong>, <strong>ploggging</strong>, des
+                <strong> jeux</strong> et <strong>ateliers éducatifs</strong>{" "}
+                initiant l’enfant au tri sélectif, au recyclage ou tout
+                simplement aux petits gestes écolos à adopter au quotidien..
+              </p>
+            </div>
+          </div>
+          <br />
+          <div className="columns">
+            <div className="column">
+              <p
+                style={{
+                  marginTop: "14%",
+                  textIndent: "20px",
+                  lineHeight: "35px",
+                }}
+              >
+                Depuis sa création, le comité veille à diversifier ses activités
+                à destination des enfants, et ce en effectuant des
+                <strong> collaborations </strong> avec d’autres parties
+                engagées, des <strong> campagnes de sensibilisation</strong>,{" "}
+                <strong>plantation </strong>
+                et <strong> randonnés</strong> en collaboration avec la
+                direction des forêts, des événements avec un club d’équestre
+                dédié aux enfants autistes, ainsi qu’un événement au niveau de
+                l’orphelinat avec la participation d’une docteur vétérinaire
+                pour la sensibilisation à la pollution et la cause animale
+                également .
+              </p>
+              <br />
+              <br />
+            </div>
+            <div className="column is-6">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+          </div>
+          <br />
+
+          <div className="columns">
+            <div className="column is-6">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+            <div className="column">
+              <p style={{ textIndent: "20px", lineHeight: "35px" }}>
+                Le comité compte à présent environ{" "}
+                <strong>200 enfants sensibiliséss </strong>, toutes catégorie
+                confondues, et ambitionne toucher davantage d’enfants en
+                élargissant le champ d’action des prochains événements.
+                <br />
+                <br />
+                <strong>L’enfant d’aujourd’hui est l’adulte de demain </strong>,
+                si nous arrivons à inclure les bonnes valeurs d’un citoyen
+                éco-responsable, nous aurons tout gagné, telle est l’ambition du
+                comité.
+                <br />
+                <br />
+                Si vous êtes dynamique, aimant le travail d’équipe et le travail
+                avec les enfants, vous n’avez qu’à nous rejoindre !
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Junior;
--- src/routes/comites/sport/sport.jsx
+++ src/routes/comites/sport/sport.jsx
@@ -1,7 +1,83 @@
 import React from "react";
 
 const Sport = () => {
-  return <section>lorem ipsum</section>;
+  return (
+    <section className="hero is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-6">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+            <div className="column">
+              <h1 className="title is-4" style={{ marginTop: "14%" }}>
+                Comité Sport et bien être :
+              </h1>
+              <p style={{ textIndent: "20px", lineHeight: "35px" }}>
+                Créé par Plogging Association Algeria, le comité sport est bien
+                être est un des <strong>organes</strong> de l’association, qui
+                combine <strong>l’activité sportive</strong> et la
+                <strong> responsabilité environnementale</strong> dans le cadre
+                du PLOGGING, et ce en organisant tout au long de l’année des
+                activités sportives et physiques diversifiées afin de proposer
+                des programmes adaptés aux capacités, aux besoins ainsi qu’au
+                bien être des différents participants.
+              </p>
+              <br />
+              <br />
+              <p style={{ textIndent: "20px", lineHeight: "35px" }}>
+                <strong>Footing</strong>, <strong>Ploggathon</strong>,{" "}
+                <strong>Bootcamp </strong>
+                et autre, ces derniers sont toujours accompagnés de séances de
+                <strong> nettoyage </strong> des lieux ou ce que nous appelons
+                communément les <strong> séances de PLOGGING</strong>, le but
+                étant de sensibiliser un maximum de monde à la cause
+                environnementale, le comité œuvre à l’atteindre à travers le
+                sport.
+              </p>
+            </div>
+          </div>
+          <br />
+          <div className="columns">
+            <div className="column">
+              <p
+                style={{
+                  marginTop: "14%",
+                  textIndent: "20px",
+                  lineHeight: "35px",
+                }}
+              >
+                Depuis sa création, le comité ne cesse de{" "}
+                <strong>recruter</strong> de nouveaux membres et volontaires,
+                passionnés par le sport et conscients de l’importance{" "}
+                <strong>d’agir </strong>
+                pour protéger l’environnement, les membres{" "}
+                <strong>veillent </strong> à l’organisation et à la réussite de
+                chaque événement du comité,{" "}
+                <strong>
+                  « Un esprit sain dans un corps sain sur une planète saine »
+                </strong>{" "}
+                telle est la devise qui les relient.
+                <br />
+                <br />
+                Si vous partagez cette devise, le comité sport et bien être est
+                fait pour vous, rejoignez-nous vite !
+              </p>
+              <br />
+              <br />
+            </div>
+            <div className="column is-6">
+              <figure class="image">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Sport;
