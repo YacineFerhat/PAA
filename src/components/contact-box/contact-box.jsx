5ce7227d97555916016493e082d617597e3b1425
Yacine Ferhat <Yacine.frhat@gmail.com>
2020-07-07 17:03:26 +0100

    FIxed contact Page

--- src/components/contact-box/contact-box.jsx
+++ src/components/contact-box/contact-box.jsx
@@ -7,14 +7,17 @@ import EmailIcon from "@material-ui/icons/Email";
 import RssFeedIcon from "@material-ui/icons/RssFeed";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 const useStyles = makeStyles({
   root: {
     display: "flex",
     flexDirection: "column",
-    height: "600px",
+    minHeight: 400,
+    minWidth: 275,
   },
-  icon: {
+  iconTitle: {
     marginTop: "3%",
     color: "#4caf50",
     fontSize: "45px",
@@ -22,25 +25,90 @@ const useStyles = makeStyles({
   desc: {
     marginLeft: "10%",
     marginRight: "10%",
+    height: 100,
   },
-  typo: {},
+  typo: {
+    wordWrap: "break-word",
+    color: "#4caf50",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  icons: {
+    display: "flex",
+    justifyContent: "space-around",
+    color: "#4caf50",
+    margin: "0% 20%",
+  },
+  links: {
+    color: "#4caf50",
+    "&:hover": {
+      color: "#234915",
+    },
+  },
+
+  icon: { fontSize: 34 },
 });
+
 const ContactBox = ({ data: { title, desc, data, sub } }) => {
   const classes = useStyles();
-
+  const bull = <span className={classes.bullet}>•</span>;
+  let socialMedia = title === "Email" ? false : true;
+  console.log(data);
   return (
-    <Paper className={`{classes.root} has-text-centered`} elevation={3}>
-      {title === "Email" ? (
-        <EmailIcon className={classes.icon} />
-      ) : (
-        <RssFeedIcon className={classes.icon} />
-      )}
-      <Typography className={classes.typo} variant="h4" gutterBottom>
-        {title}
-      </Typography>
-      <Typography className={classes.desc} variant="h6" gutterBottom>
-        {desc}
-      </Typography>
+    <Paper elevation={3} className="has-text-centered">
+      <Card className={classes.root}>
+        <CardContent>
+          {socialMedia ? (
+            <RssFeedIcon className={classes.iconTitle} />
+          ) : (
+            <EmailIcon className={classes.iconTitle} />
+          )}
+          <Typography variant="h4" gutterBottom>
+            {title}
+          </Typography>
+          <Typography className={classes.desc} variant="h6" gutterBottom>
+            {desc}
+          </Typography>
+          {socialMedia ? (
+            <div className={`${classes.icons} has-text-centered`}>
+              <a
+                className={classes.links}
+                href="https://www.youtube.com/channel/UC9NDD6jwR0GdiJUodVdQesA*/"
+                target="_blank"
+              >
+                <YouTubeIcon className={classes.icon} />
+              </a>
+              <a
+                className={classes.links}
+                href="https://www.instagram.com/plogging_association_algeria/?hl=fr"
+                target="_blank"
+              >
+                <InstagramIcon className={classes.icon} />
+              </a>
+              <a
+                className={classes.links}
+                href="https://www.facebook.com/PloggingAssociationAlgeria/"
+                target="_blank"
+              >
+                <FacebookIcon className={classes.icon} />
+              </a>
+            </div>
+          ) : (
+            <Typography className={classes.typo} variant="h5" gutterBottom>
+              {data}
+            </Typography>
+          )}
+        </CardContent>
+      </Card>
     </Paper>
   );
 };
